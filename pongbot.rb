d8836a3638ae13785dddd5120722ae8dfc28b10c
Cliff Rodgers <kliph@users.noreply.github.com>
2016-04-14 14:19:44 -0500

    Reward users for interacting with the bot

--- pongbot.rb
+++ pongbot.rb
@@ -3,6 +3,9 @@ require 'slack-ruby-bot'
 class PongBot < SlackRubyBot::Bot
   command 'ping' do |client, data, match|
     client.say(text: 'pong', channel: data.channel)
+    client.say(text: 'Thanks for participating.',
+               gif: %w(thanks sweet kudos cheers).sample,
+               channel: data.channel)
   end
 end
 
